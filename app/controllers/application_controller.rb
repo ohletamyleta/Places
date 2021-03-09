24c2ee18e7491276cd62fbfde725071aff5ac111
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 12:42:06 -0500

    add controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
 
-  def login
-  end 
+  before_action :redirect_if_not_logged_in
+
+  helper_method :current_user, :logged_in?
+
+
+  private
 
   def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to login_path if !logged_in?
+  end
   
 end
