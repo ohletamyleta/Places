1cd5ac542ec3779e4613d79fdb4edf56bccb50bd
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 10:06:50 -0500

    add login method and views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def new 
-    
   end
 
   def create 
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password]) 
+      session[:user_id] = @user.id 
+        redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
   end
+
   
   def destroy 
     session.clear
