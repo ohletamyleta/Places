95c7a39ae6446f9fa5d128edb32b23b40f188b9c
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 12:50:29 -0500

    build show views and form

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,10 +1,16 @@
 class ShowsController < ApplicationController
 
   def index 
+    @shows = Show.all
   end
 
   def new
+    @show = Show.new
   end
+
+  def create 
+
+  end 
 
   def show 
   end 
