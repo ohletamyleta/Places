181ee8df91c68a6d3114674437e9f41be4cd1d4f
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 13:35:15 -0500

    build controller actions for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,12 +1,24 @@
 class ActorsController < ApplicationController
 
   def index 
+    @actors = Actor.all
   end
 
   def new
+    @actor = Actor.new
+  end
+
+  def create 
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to actor_path(@actor)
+    else
+      render :new
+    end
   end
 
   def show 
+    @actor = Actor.find_by(id: params[:id])
   end 
 
   def edit
@@ -18,6 +30,11 @@ class ActorsController < ApplicationController
   def destroy 
   end
 
+private
 
+  def actor_params
+    params.require(:actor).permit(:first_name, :last_name, :role)
+  end 
 
+  
 end
