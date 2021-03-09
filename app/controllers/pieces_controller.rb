2abf9127dd299611c13a2252566c0ade1da79e3e
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 16:03:36 -0500

    add actions and views to pieces controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,12 +1,24 @@
 class PiecesController < ApplicationController
 
   def index 
+    @pieces = Piece.all 
   end
 
   def new
+    @piece = Piece.new
   end
 
+  def create 
+    @piece = Piece.new(piece_params)
+    if @piece.save
+      redirect_to piece_path(@piece)
+    else
+      render :new
+    end
+  end
+  
   def show 
+    @piece = Piece.find_by(id: params[:id])
   end 
 
   def edit
@@ -17,6 +29,13 @@ class PiecesController < ApplicationController
 
   def destroy 
   end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:name, :tag_number, :type, :color, :fabric, :time_period, :ok_to_alter, :storage_location, :cleaning_method, :notes,
+      :waist, :hip, :shoulder_to_shoulder, :chest, :neck, :neck_to_waist, :waist_to_hem, :sleeve_length, :inseam, :tag_size)
+  end 
 
 
 end
