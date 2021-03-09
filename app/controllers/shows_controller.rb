aebeba48c7d96703af9d9f97e85036deff91dfac
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 13:05:03 -0500

    add show and index views to Shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,7 +1,7 @@
 class ShowsController < ApplicationController
 
   def index 
-    @shows = Show.all
+    @shows = current_user.shows
   end
 
   def new
@@ -9,10 +9,17 @@ class ShowsController < ApplicationController
   end
 
   def create 
-
+    @show = current_user.shows.build(show_params)
+    if @show.save
+      redirect_to show_path(@show)
+    else
+      render :new
+    end
   end 
 
   def show 
+    @show = Show.find_by(id: params[:id])
+    
   end 
 
   def edit
@@ -24,5 +31,10 @@ class ShowsController < ApplicationController
   def destroy 
   end
 
+  private
+
+  def show_params
+    params.require(:show).permit(:title, :start_date, :end_date)
+  end 
 
 end
