6fe54b718719941f4f7c8cff03943226f99095f9
ohletamyleta <ohletamyleta@gmail.com>
2021-03-09 19:59:06 -0500

    edit create piece method and form

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -10,6 +10,7 @@ class PiecesController < ApplicationController
 
   def create 
     @piece = Piece.new(piece_params)
+    #byebug
     if @piece.save
       redirect_to piece_path(@piece)
     else
