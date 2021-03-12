43cc8164b996a05045170aed967f6592a1a19b55
ohletamyleta <ohletamyleta@gmail.com>
2021-03-11 21:00:00 -0500

    adding association definition between user/costume

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :shows
-  has_many :costumes
+  has_many :built_costumes, foreign_key: "user_id", class_name: "Costume"
   
   validates :name, :email, presence: true 
   validates :name, :email, uniqueness: true
